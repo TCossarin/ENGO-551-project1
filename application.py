8a659c84a01c81a3322033682e43e05a266c7dde
TCossarin <77802662+TCossarin@users.noreply.github.com>
2024-02-10 02:21:35 -0700

    Add files via upload

--- application.py
+++ application.py
@@ -1,174 +1,120 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+# Initialize Flask app
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'
+app.secret_key = 'secret_key'
 
-password = '12345'
-owner_name = 'postgres'
-database_name = 'postgres'
-DATABASE_URL = 'postgresql://'+ owner_name +':'+ password +'@localhost/' + database_name
-print('database:', DATABASE_URL)
-
-engine = create_engine(DATABASE_URL)
+# Database setup
+engine = create_engine('postgresql://postgres:12345@localhost/postgres')
 db = scoped_session(sessionmaker(bind=engine))
-print('Database set up')
 
-with engine.connect() as eng:
-    for row in eng.execute("select * from books limit 1 offset 1;"):
-        print(row)
-
-
-
-users = {}
 
 @app.route('/')
-def home():
-    if 'username' in session:
-        return redirect(url_for('secure'))
-    return redirect(url_for('login'))
+def home():                             # Home page route - redirects to login page if user is not logged in
+    if 'username' in session:           # If user is logged in, show the home page
+        return render_template('home.html')
+    
+    return redirect(url_for('login'))   # Back to login page if no user is logged in
 
 
 @app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
+def login():                                # Login route to authenticate users
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+        # Get user from the database based on username and password
+        user = db.execute("SELECT username FROM users WHERE username = :username AND password = :password",
+                          {"username": request.form['username'], "password": request.form['password']}).fetchone()
         
-        # Query the database to check if the username and password match
-        user = db.execute("SELECT username FROM users WHERE username = :username AND password = :password", {"username": username, "password": password}).fetchone()
-        if user:
-            # Authentication successful, set session and redirect to secure page
-            session['username'] = username
+        if user:                            # If user exists, start session, redirect to home
+            session['username'] = user.username
             flash('Login successful.', 'success')
-            return redirect(url_for('secure'))
-        else:
-            # Authentication failed, display error message
-            error = 'Invalid username or password'
-
-    return render_template('login.html', error=error)
+            return redirect(url_for('home'))
+        
+        else:                               # Login failed
+            flash('Invalid username or password', 'error')
+    
+    return render_template('login.html')    # Show login form
 
 
 @app.route('/register', methods=['GET', 'POST'])
-def register():
-    error = None
+def register():                                 # Registration route for new users
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        
-        # Check if the username already exists in the database
-        existing_user = db.execute("SELECT username FROM users WHERE username = :username", {"username": username}).fetchone()
-        if existing_user:
-            error = 'Username already taken, please choose another'
-        else:
-            # Insert new user into the database
-            db.execute("INSERT INTO users (username, password) VALUES (:username, :password)", {"username": username, "password": password})
+        # Check if username already exists in the database
+        if db.execute("SELECT username FROM users WHERE username = :username", {"username": request.form['username']}).fetchone():
+            flash('Username already taken, please choose another', 'error')     # Username taken
+
+        else:                                   # If username is available, add new user to database
+            db.execute("INSERT INTO users (username, password) VALUES (:username, :password)",
+                       {"username": request.form['username'], "password": request.form['password']})
             db.commit()
             flash('Registration successful. You can now log in.', 'success')
             return redirect(url_for('login'))
-
-    return render_template('register.html', error=error)
-
-
-
-@app.route('/secure')
-def secure():
-    if 'username' not in session:
-        return redirect(url_for('login'))
     
-    # Execute SQL query
-    book_row = None
-    with engine.connect() as connection:
-        book_row = connection.execute("SELECT * FROM books LIMIT 1 OFFSET 1;").fetchone()
-    return render_template('secure.html', username=session['username'], book_row=book_row)
+    return render_template('register.html')     # Show registration form
 
 
 @app.route('/logout')
-def logout():
+def logout():                                   # Logout route to end session
     session.pop('username', None)
-    return redirect(url_for('login'))
+    flash('You have been logged out.', 'info')
+    
+    return redirect(url_for('login'))           # Return to login
 
 
 @app.route('/search')
-def search():
+def search():                                   # Search route for books in database
     query = request.args.get('query')
-    if query:
-        query = f"%{query}%"
-        sql_query = text("SELECT * FROM books WHERE isbn ILIKE :query OR title ILIKE :query OR author ILIKE :query")
-        results = db.execute(sql_query, {"query": query}).fetchall()
-        return render_template('search_results.html', results=results)
-    return redirect(url_for('secure'))
+    results = db.execute("SELECT * FROM books WHERE isbn ILIKE :query OR title ILIKE :query OR author ILIKE :query",
+                         {"query": f"%{query}%"}).fetchall() if query else []
+    
+    if results:
+        return render_template('search_results.html', results=results)  # Show search results if query exists
+    return redirect(url_for('home'))                                    # Redirect to home
 
 
 @app.route('/book/<isbn>', methods=['GET', 'POST'])
-def book(isbn):
-    # Retrieve book information
+def book(isbn):                                     # Route to display details of a specific book including reviews
     book = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": isbn}).fetchone()
-
-    # Retrieve existing reviews for the book
     reviews = db.execute("SELECT * FROM reviews WHERE isbn = :isbn", {"isbn": isbn}).fetchall()
 
-    # Check if the current user has already submitted a review for this book
-    review_exists = False
-    if 'username' in session:
-        username = session['username']
-        review_exists = db.execute("SELECT * FROM reviews WHERE isbn = :isbn AND username = :username", {"isbn": isbn, "username": username}).fetchone()
-
+    if 'username' in session:                       # information to display in book.html
+        review_exists = db.execute("SELECT * FROM reviews WHERE isbn = :isbn AND username = :username",
+                                {"isbn": isbn, "username": session.get('username')}).fetchone()
+    else:
+        review_exists = False
+    
     return render_template('book.html', book=book, reviews=reviews, review_exists=review_exists)
 
 
 @app.route('/submit_review/<isbn>', methods=['POST'])
-def submit_review(isbn):
-    if request.method == 'POST':
-        # Get username from session
-        username = session.get('username')
+def submit_review(isbn):                                # Route for submitting a review
+    if 'username' in session and request.method == 'POST':
+        # Check if user already has a review
+        if not db.execute("SELECT * FROM reviews WHERE isbn = :isbn AND username = :username",
+                          {"isbn": isbn, "username": session['username']}).fetchone():
+            # Add new review to database
+            db.execute("INSERT INTO reviews (isbn, username, comment, rating) VALUES (:isbn, :username, :comment, :rating)",
+                       {"isbn": isbn, "username": session['username'], "comment": request.form['comment'], "rating": request.form['rating']})
+            db.commit()
+            flash('Review submitted successfully.', 'success')
 
-        # Check if the user has already submitted a review for this ISBN
-        existing_review = db.execute("SELECT * FROM reviews WHERE isbn = :isbn AND username = :username", {"isbn": isbn, "username": username}).fetchone()
-        if existing_review:
+        else:
             flash('You have already submitted a review for this book.', 'warning')
-            return redirect(url_for('book', isbn=isbn))
-
-        # If not, proceed with submitting the new review
-        comment = request.form['comment']
-        rating = request.form['rating']
-
-        # Save the review to the database
-        db.execute("INSERT INTO reviews (isbn, username, comment, rating) VALUES (:isbn, :username, :comment, :rating)", {"isbn": isbn, "username": username, "comment": comment, "rating": rating})
-        db.commit()
-
-        flash('Review submitted successfully.', 'success')
-
-        # Redirect back to the book page
-        return redirect(url_for('book', isbn=isbn))
+    return redirect(url_for('book', isbn=isbn))
 
 
 @app.route('/delete_review/<isbn>', methods=['POST'])
-def delete_review(isbn):
-    if 'username' not in session:
+def delete_review(isbn):                                # Route for deleting a user's review
+    if 'username' in session:
+        # Delete review from database
+        db.execute("DELETE FROM reviews WHERE isbn = :isbn AND username = :username",
+                   {"isbn": isbn, "username": session['username']})
+        db.commit()
+        flash("Your review has been successfully deleted.", 'success')
+    else:
         flash('You must be logged in to delete a review.', 'error')
-        return redirect(url_for('login'))
-
-    # Get the current user's username from the session
-    username = session['username']
-
-    # Check if the user has already submitted a review for this book
-    existing_review = db.execute("SELECT * FROM reviews WHERE isbn = :isbn AND username = :username", {"isbn": isbn, "username": username}).fetchone()
-    if not existing_review:
-        flash("You haven't submitted a review for this book.", 'error')
-        return redirect(url_for('book', isbn=isbn))
-
-    # Delete the review from the database
-    db.execute("DELETE FROM reviews WHERE isbn = :isbn AND username = :username", {"isbn": isbn, "username": username})
-    db.commit()
-
-    flash("Your review has been successfully deleted.", 'success')
-
-    # Redirect back to the book page
     return redirect(url_for('book', isbn=isbn))
-
 
 
 if __name__ == '__main__':
